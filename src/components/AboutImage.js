0a0d2485a5801619635b97a320a0b936e17138e7
Tyler de Friess <tyler.defriess@gmail.com>
2020-01-23 19:05:08 -0500

    Some Styled and ReactStrap components added. Tried, failed to implement collapsing box.

--- src/components/AboutImage.js
+++ src/components/AboutImage.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
+import styled from 'styled-components';
+
+const AboutDiv = styled.div`
+    width: 50%;
+`
 
 export default function AboutImage(props) {
     return (
-        <div className='description-box'>
+        <AboutDiv>
             <h4>{props.picture.title}</h4>
             <h6>Date: {props.picture.date}</h6>
             <p><strong>Description: </strong>{props.picture.explanation}</p>
-        </div>
+            <Form>
+                <FormGroup>
+                    <Label>Choose Another Date: </Label>
+                    <Input
+                        type='date'
+                        name='time'
+                        id='exampleTime'
+                        placeholder='time placeholder'
+                    />                    
+                </FormGroup>
+            </Form>
+        </AboutDiv>
     )
 }
